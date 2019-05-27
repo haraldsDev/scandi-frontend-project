d12da95abee582379177dcf7b3e680e65c9cd603
haraldsDev <Haralds.Dev@gmail.com>
2019-05-27 17:20:59 +0300

    added 2 Icon components to chekc how it works : )  - didn't quite work

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import './App.css';
+import StatisticIcon from './initial-assets/extracted-assets-h/StatisticIcon.js';
+import SecondIcon from './initial-assets/extracted-assets-h/SecondIcon.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          This is a FRONTEND test project <br /> for Scandiweb <br /> by Haralds Matulis.
-        </p>
- 
-      </header>
+    <div>
+	    <div className="App">
+	      <header className="App-header">
+	        <p>
+	          This is a FRONTEND project <br /> for Scandiweb <br /> by Haralds Matulis.
+	        </p>
+	      </header>
+	      
+
+	    </div>
+	    <StatisticIcon />
+	    <SecondIcon />
     </div>
   );
 }
