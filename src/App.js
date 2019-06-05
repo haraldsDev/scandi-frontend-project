e80bbe3327a59858571a55c54640ed911679b30b
haraldsDev <Haralds.Dev@gmail.com>
2019-06-05 13:43:40 +0300

    put BottomBar component inside App to render

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import './App.css';
+import BottomBar from './BottomBar/BottomBar';
 
 function App() {
   return (
     <div>
 	    <div className="App">
-	      <header className="App-header">
-	        <p>
-	          This is a FRONTEND project <br /> for Scandiweb <br /> by Haralds Matulis.
-	        </p>
-	      </header>
+	    	<BottomBar />
 	    </div>
     </div>
   );
