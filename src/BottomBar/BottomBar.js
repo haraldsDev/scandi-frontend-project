36ca4f0e7276c3acd7da6935bd3b6e392de09236
haraldsDev <Haralds.Dev@gmail.com>
2019-06-05 13:42:24 +0300

    made dimensions and text for BottomBar on FeedView

---
+++ src/BottomBar/BottomBar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './BottomBar.css';
+
+const BottomBar = () => {
+	return (
+		<div className="bottom-bar white bt bw1 b--white-40">
+			<p className="center"> here will be BottomBar </p>
+		</div>
+
+	);
+}
+
+export default BottomBar;
