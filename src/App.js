61f22327600e04d027838c5496dcb227e1d9bd97
haraldsDev <Haralds.Dev@gmail.com>
2019-06-06 17:50:00 +0300

    added FeedView to render

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import BottomBar from './BottomBar/BottomBar';
+import FeedView from './containers/FeedView/FeedView';
 
 function App() {
   return (
     <div>
 	    <div className="App">
+	    	<FeedView />
 	    	<BottomBar />
 	    </div>
     </div>
