92ff33b7e901a055b42c4ea6ccd8b26436af1a07
haraldsDev <Haralds.Dev@gmail.com>
2019-06-09 19:37:06 +0300

    styled user-bar, added userpics, added opacity to background-image

--- src/containers/FeedView/FeedView.js
+++ src/containers/FeedView/FeedView.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './FeedView.css';
 import backgroundImage from '../../initial-assets/background-image.png';
+import userpic1 from '../../initial-assets/userpic1.png';
+import userpic2 from '../../initial-assets/userpic2.png';
 
 const FeedView = () => { 
 	return (
 		<div className="feed-view white">
-			<div className="background-image shp0" >
-				<img src={backgroundImage} alt=""/> 
+			<div className="background-image shp0 shp36 " >
+				<img src={backgroundImage} alt=""/>  
 			</div>
 			<div classname="upper-two-thirds">
 				<div className="top-line mt4 ml4">top-line</div>
@@ -19,7 +21,7 @@ const FeedView = () => {
 				<div>
 					<div id="Users">
 						
-						<div className="flex flex-wrap mt3 mb4">
+						<div className="flex flex-wrap mt3 mb3">
 							<div className="flex flex-wrap flex-start w-80">
 								<div id="By" className="txt41 ml3 mr1">By </div>
 								<div id="Nocholas-O-Brian" className="txt40">Nocholas O`Brian</div>
@@ -27,10 +29,16 @@ const FeedView = () => {
 							<div id="28 July" className="txt40 flex-end w-20">28 July</div>
 						</div>			
 						<div className="flex flex-wrap flex-start mb3">
-							<div id="Userpic1" className="ml3 mr2">pic1</div>
-							<div id="Userpic2" className="mr2">pic2</div>
-							<div id="+76" className="txt39 mr2">+76</div>
-							<div id="Liked-this opacity: 0.6" className="txt7 mr2">Liked this</div>
+							<div id="Userpic1" className="ml3 mr2">
+								<img src={userpic1} alt="userpic1"/>
+							</div>
+							<div id="Userpic2" className="mr2">
+								<img src={userpic2} alt="userpic2" />
+							</div>
+							<div id="+76" className="number-76 shp30 mr2 txt39">
+								<div className="ml2 mt1">+76</div>
+							</div>
+							<div id="opacity: 0.6" className="liked-this mt1 txt7 mr2">Liked this</div>
 						</div>
 						
 					</div>
