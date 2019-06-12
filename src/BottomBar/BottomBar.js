f6a8d9974f295ac389504372e9f08f93020fbe6b
haraldsDev <Haralds.Dev@gmail.com>
2019-06-12 19:43:36 +0300

    added 5 icons to the bottomBar, styled, flex, inactive

--- src/BottomBar/BottomBar.js
+++ src/BottomBar/BottomBar.js
@@ -1,56 +1,35 @@
 import React from 'react';
 import './BottomBar.css';
-import baselinePerson from '../initial-assets/icons/baselinePerson.svg';
-
-
+import person from '../initial-assets/icons/person.svg';
+import statistics1 from '../initial-assets/icons/statistics1.svg';
+import notifications from '../initial-assets/icons/notifications.svg';
+import bookmark from '../initial-assets/icons/bookmark.svg';
+import home from '../initial-assets/icons/home.svg';
 
 const BottomBar = () => { 
 	return (
 		<div className="bottom-bar shp33 bt bw1 b--white-05">
-			
-			<div className="icons flex flex-wrap">
+			<div className="icons flex justify-around mt3">
+				<div className=""> 
+					<img src={person} alt="" className="pointer person inactive"/>
+				</div>
+				<div className=""> 
+					<img src={statistics1} alt="" className="pointer statistics1 active"/>
+				</div>
+				<div className=""> 
+					<img src={notifications} alt="" className="pointer notifications inactive"/>
+				</div>
+				<div className=""> 
+					<img src={bookmark} alt="" className="pointer bookmark inactive"/>
+				</div>
 
 				<div className=""> 
-					<img src={baselinePerson} alt="" className="pointer baselinePerson"/>
-				</div>
-				<div className=""> 
-					<img src={baselinePerson} alt="" className="pointer baselinePerson"/>
-				</div>
-				<div className=""> 
-					<img src={baselinePerson} alt="" className="pointer baselinePerson"/>
-				</div>
-				<div className=""> 
-					<img src={baselinePerson} alt="" className="pointer baselinePerson"/>
-				</div>
-				<div className=""> 
-					<img src={baselinePerson} alt="" className="pointer baselinePerson"/>
-				</div>
-				<div className=""> 
-					<img src={baselinePerson} alt="" className="pointer baselinePerson"/>
+					<img src={home} alt="" className="pointer home inactive"/>
 				</div>
 			</div>
-
-
 		</div>
 
 	);
 }
 
 export default BottomBar;
-
-
-/*
-
-		<g id="Bottom Bar">
-			<path id="Background" class="shp33" d="M101,1582h375v-52h-375z" />
-			<path id="Icon" class="shp34" d="M139 1558.5C135.66 1558.5 129 1560.18 129 1563.5L129 1566L149 1566L149 1563.5C149 1560.18 142.34 1558.5 139 1558.5M139 1546C136.24 1546 134 1548.24 134 1551C134 1553.76 136.24 1556 139 1556C141.76 1556 144 1553.76 144 1551C144 1548.24 141.76 1546 139 1546" />
-			<path id="Icon" class="shp9" d="M213.48 1563.05L213.48 1556.66L220.05 1556.66L220.05 1561.96C220.05 1562.56 219.56 1563.05 218.96 1563.05L213.48 1563.05ZM211.52 1547.95L211.52 1554.71L204.95 1554.71L204.95 1549.04C204.95 1548.44 205.44 1547.95 206.04 1547.95L211.52 1547.95ZM203 1548.17L203 1562.83C203 1564.03 203.97 1565 205.17 1565L219.83 1565C221.03 1565 222 1564.03 222 1562.83L222 1548.17C222 1546.97 221.03 1546 219.83 1546L205.17 1546C203.97 1546 203 1546.97 203 1548.17L203 1548.17Z" />
-			<path id="Icon" class="shp34" d="M290.58 1563.85L290.58 1563.85M290.58 1563.85L286.32 1563.85C286.32 1565.03 287.27 1566 288.45 1566C289.62 1566 290.58 1565.03 290.58 1563.85M288.45 1545C287.57 1545 286.86 1545.72 286.86 1546.62L286.86 1547.35C283.8 1548.08 282.07 1550.84 282.07 1554.15L282.07 1559.54L279.94 1561.69L279.94 1562.77L296.96 1562.77L296.96 1561.69L294.83 1559.54L294.83 1554.15C294.83 1550.85 293.09 1548.08 290.05 1547.35L290.05 1546.62C290.05 1545.72 289.33 1545 288.45 1545" />
-			<path id="Icon" class="shp34" d="M369.2,1546h-11.4c-0.99,0 -1.8,0.81 -1.8,1.81v15.91c0,0.91 0.53,1.73 1.34,2.09c0.28,0.12 0.57,0.18 0.86,0.18c0.72,0 1.42,-0.36 1.84,-1.01l3.47,-5.38l3.45,5.35c0.42,0.65 1.12,1.02 1.84,1.02c0.29,0 0.58,-0.06 0.86,-0.18c0.81,-0.36 1.34,-1.18 1.34,-2.09v-15.89c0,-1 -0.81,-1.81 -1.8,-1.81" />
-			<path id="Icon" class="shp34" d="M446,1565.99c1.11,0 2,-0.9 2,-2.02v-10.63c0,-0.65 -0.31,-1.26 -0.84,-1.64l-7.58,-5.46c-0.35,-0.25 -0.81,-0.25 -1.16,0l-7.58,5.46c-0.53,0.38 -0.84,0.99 -0.84,1.64v10.63c0,1.11 0.9,2.02 2,2.02h14" />
-			<path id="Line" class="shp35" d="M101,1530.5h375v-0.5h-375z" />
-		</g>
-
-*/
-
-	
--- src/containers/FeedView/FeedView.js
+++ src/containers/FeedView/FeedView.js
@@ -5,7 +5,6 @@ import userpic1 from '../../initial-assets/userpic1.png';
 import userpic2 from '../../initial-assets/userpic2.png';
 import arrowBack from '../../initial-assets/icons/arrowBack.svg';
 
-
 const FeedView = () => { 
 	return (
 		<div className="feed-view white">
@@ -22,11 +21,9 @@ const FeedView = () => {
 				<div className="who-are-you txt43 mb1">Who are you and <br /> why you're <br />standing here?</div>
 				<div className="it-doesnt-matter txt42 mt0">It doesn't matter if you're building the next Facebook, or if you're the director of marketing for an industrial company.</div>
 			</div>
-
 			<div className="user-bar bt bw1 b--white-05">
 				<div>
-					<div id="Users">
-						
+					<div id="Users">	
 						<div className="flex flex-wrap mt3 mb3">
 							<div className="flex flex-wrap flex-start w-80">
 								<div id="By" className="txt41 ml3 mr1">By </div>
@@ -46,51 +43,12 @@ const FeedView = () => {
 							</div>
 							<div id="opacity: 0.6" className="liked-this mt1 txt7 mr2">Liked this</div>
 						</div>
-						
 					</div>
-	
-
 				</div>
 			</div>
-
-			<p className="center yellow"></p>
 		</div>
-
 	);
 }
 
 export default FeedView;
-
-
-
-/*
-<div className="it-doesnt-matter txt42 mt0">It doesn't matter if you're building the next Facebo <br /> ok, or if you're the director of marketing for an industrial company.</div>
-*/
-
-/*
-
-<g id="Feed #1" clip-path="url(#cp3)">
-		
-
-
-		<path id="Line" class="shp35" d="M116,1420.5h343v-0.5h-343z" />
-		<text id="It doesn't matter if you're building the next Facebook, or if y" style="transform: matrix(1,0,0,1,117.168,1331.316)" >
-			<tspan x="0" y="18" class="txt42">It doesn't matter if you're building the next Facebook, or if you're the director of marketing for an industrial company.</tspan>
-		</text>
-		<text id="Who are you and  why you’re  standing here?" style="transform: matrix(1,0,0,1,117.168,1191.316)" >
-			<tspan x="0" y="40" class="txt43">Who are you and </tspan>
-			<tspan x="0" y="84.8" class="txt43">why you’re </tspan>
-			<tspan x="0" y="129.6" class="txt43">standing here?</tspan>
-		</text>
-		<g id="Topline">
-			<g id="Back">
-				<path id="Ellipse" class="shp22" d="M117,973.5c0,11.87 9.63,21.5 21.5,21.5c11.87,0 21.5,-9.63 21.5,-21.5c0,-11.87 -9.63,-21.5 -21.5,-21.5c-11.87,0 -21.5,9.63 -21.5,21.5z" />
-				<path id="Arrow" class="shp9" d="M134.74,973.53l6.91,-7.17c0.29,-0.3 0.29,-0.78 0,-1.07c-0.29,-0.3 -0.75,-0.3 -1.03,0l-7.42,7.71c-0.29,0.3 -0.29,0.77 0,1.07l7.42,7.71c0.14,0.15 0.33,0.22 0.52,0.22c0.19,0 0.37,-0.07 0.52,-0.22c0.29,-0.3 0.29,-0.78 0,-1.07z" />
-			</g>
-		</g>
-	</g>
-
-*/
-
-
 	 
